0157801ca12739232f3240587f8353bb4c53d8f2
Dominik Hauser <dom@dasdom.de>
2021-04-08 22:39:37 +0200

    Initial Commit

---
+++ LatexDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//  Created by Dominik Hauser on 08/04/2021.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
